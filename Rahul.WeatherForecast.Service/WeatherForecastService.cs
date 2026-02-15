78cf013051b47eecf392422aacca189bd820745f
Rahul Ragi <rahulragi30@gmail.com>
2026-02-15 10:30:28 -0500

    Add new ASP.NET Core web API project for weather data
    This commit introduces the `Rahul.NavyServices.API` project, which includes a `WeatherForecastController` for providing weather forecast data. The project targets .NET 9.0 and integrates OpenAPI for API documentation. The `WeatherForecast` model is sourced from the `Rahul.WeatherForecast.Service` project. Configuration files such as `appsettings.json` and `launchSettings.json` have been updated accordingly. Additionally, modifications to `.gitattributes` and `.gitignore` reflect the new project structure and Visual Studio settings.

---
+++ Rahul.WeatherForecast.Service/WeatherForecastService.cs
@@ -0,0 +1,27 @@
+﻿namespace Rahul.WeatherForecast.Service
+{
+    public class WeatherForecastService
+    {
+        private static readonly string[] Summaries = new[]
+               {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+        public  WeatherForecastService()
+        {
+
+        }
+        public IEnumerable<WeatherForecast> GetWeatherForecasts() 
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+
+    }
+
+}
